c9404f24d2436d9d0545a8369718c47bc40bcca0
lucas-catto <lucascattosilva2018@gmail.com>
2024-08-20 19:47:32 -0300

    Nova versão (exemplo do professor).

--- app-flat-list/App.js
+++ app-flat-list/App.js
@@ -1,94 +1,26 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// React
-import React from 'react';
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
-// React Native
-import {
-    SafeAreaView,
-    View,
-    FlatList,
-    StyleSheet,
-    Text,
-    StatusBar,
-    Image,
-} from 'react-native';
+const Stack = createNativeStackNavigator();
 
-// React Native Paper
-import {
-    List
-} from 'react-native-paper';
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-// Example of data
-const DATA = [
-    {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        titulo: '1 Item',
-        descricao: 'Descrição do Item 1',
-    },
-    {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        titulo: '2 Item',
-        descricao: 'Descrição do Item 2',
-    },
-    {
-        id: '5869d4a0f-3da1-471f-bd96-145571e29d72',
-        titulo: '3 Item',
-        descricao: 'Descrição do Item 3',
-    }
-    
-];
-
-// Function to read title
-const Item = ({elemento: item}) => {
-    return(   
-    <List.Item
-        title={item.titulo}
-        description={item.descricao}
-        // left={props => <List.Icon {...props} icon="folder" />}
-        left={props => 
-            <Image {...props} 
-                style={styles.tinyLogo} 
-                source={
-                    {
-                        uri: 'https://cdn.pixabay.com/photo/2012/04/13/17/00/lcd-32872_1280.png'
-                    }
-                }/>}
-    />
-)};
-
-// App
-const App = () => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <FlatList
-                data={ DATA }
-                renderItem={({ item }) =><Item elemento={ item } /> }
-                keyExtractor={ item => item.id }
-            />
-        </SafeAreaView>
-    );
-};
-
-// Style
-const styles = StyleSheet.create({
-    container: {
-        flex:      1,
-        marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-        backgroundColor: '#f9c2ff',
-        padding:          20,
-        marginVertical:   8,
-        marginHorizontal: 16,
-    },
-    title: {
-        fontSize: 32,
-    },
-    tinyLogo: {
-        width: 50,
-        height: 50,
-    },
-});
-
-// Export
 export default App;
---
+++ app-flat-list/data/Produtos.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        titulo: 'Notebook Dell',
+        descricao: 'Descrição do Notebook Dell',
+        urlImagem: 'https://media.istockphoto.com/id/164637079/pt/foto/computador-port%C3%A1til.jpg?s=2048x2048&w=is&k=20&c=i5Cf6-AJIHVtb5PUmrDzjIGfukO9qXX1vbZdsGMreY0='
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        titulo: 'Celular IPhone 15',
+        descricao: 'Descrição do Celular IPhone 15',
+        urlImagem: 'https://media.istockphoto.com/id/670692276/pt/foto/blank-black-and-white-phone-case-mock-up-isolated.jpg?s=2048x2048&w=is&k=20&c=baQGkfWCbMLY_GbLgGSbORFTP8LcsYPTuT0-hpCbJ5E='
+    },
+    {
+        id: '5869d4a0f-3da1-471f-bd96-145571e29d72',
+        titulo: 'Mouse Logitech pro',
+        descricao: 'Descrição do Mouse Logitech pro',
+        urlImagem: 'https://cdn.pixabay.com/photo/2013/01/10/14/15/mouse-74533_1280.jpg'
+    }
+    
+];
